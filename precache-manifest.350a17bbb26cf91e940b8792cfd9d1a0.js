6c17a9883c5e7b883a68903d2c319ebeb596cba1
FilipCiesielski <filip.carpenter@gmail.com>
2019-10-09 19:32:04 +0200

    Updates

---
+++ precache-manifest.350a17bbb26cf91e940b8792cfd9d1a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8122d1c071de472844995717020a60ee",
+    "url": "/CodersLab_Certificates/index.html"
+  },
+  {
+    "revision": "489325838966bb2924ae",
+    "url": "/CodersLab_Certificates/static/css/main.42d5ec1f.chunk.css"
+  },
+  {
+    "revision": "650675f201e44e1f182e",
+    "url": "/CodersLab_Certificates/static/js/2.877a642e.chunk.js"
+  },
+  {
+    "revision": "489325838966bb2924ae",
+    "url": "/CodersLab_Certificates/static/js/main.6be411aa.chunk.js"
+  },
+  {
+    "revision": "579939ff621106c1c661",
+    "url": "/CodersLab_Certificates/static/js/runtime-main.f1a3282f.js"
+  },
+  {
+    "revision": "1aa5a34524240f38c7a26fc0863e42c4",
+    "url": "/CodersLab_Certificates/static/media/CL_dyplom_podstawowy_FC-1.1aa5a345.png"
+  },
+  {
+    "revision": "39d62c598c30aef35b6b7bb009165daa",
+    "url": "/CodersLab_Certificates/static/media/CL_suplement JavaScript Portfolio Lab_PL_47 (1)-1.39d62c59.png"
+  },
+  {
+    "revision": "d1c3b33e66ac00b36df4f6623a90699a",
+    "url": "/CodersLab_Certificates/static/media/CL_suplement JavaScript_PL (1)-1.d1c3b33e.png"
+  },
+  {
+    "revision": "cc2fead2c8dbfc288304218c8baab6a4",
+    "url": "/CodersLab_Certificates/static/media/CL_suplement JavaScript_PL (1)-2.cc2fead2.png"
+  }
+]);
